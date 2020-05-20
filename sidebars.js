73a8808a1f97cd4cdb502ffc3dc6e652caeb3f41
Sagar Chalise <chalisesagar@gmail.com>
2020-05-20 15:54:35 +0545

    sidebar category for naming conventions

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,47 @@
-module.exports = {
+module.exports = 
+{
   "docs": {
-    "Overview": ["introduction"],
-    "Naming Convention": ["files", "classes", "functions", "variables", "constants", "folders"]
+    "Overview": [
+      "introduction"
+    ],
+    "Python": [
+      {
+        "type": "subcategory",
+        "label": "Files, Folders and Modules",
+        "ids": [
+          "python/files"
+        ]
+      },
+      {
+        "type": "subcategory",
+        "label": "Variables",
+        "ids": [
+          "python/variables"
+        ]
+      },
+      {
+        "type": "subcategory",
+        "label": "Functions and Methods",
+        "ids": [
+          "python/functions"
+        ]
+      },
+      {
+        "type": "subcategory",
+        "label": "Classes",
+        "ids": [
+          "python/classes"
+        ]
+      }
+    ],
+    "Naming Convention": [
+      "files",
+      "classes",
+      "functions",
+      "variables",
+      "constants",
+      "folders"
+    ]
   }
-};
+}
+;
