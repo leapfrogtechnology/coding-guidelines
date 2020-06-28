af44f1a44b05e75f8f661ebc0388b3b9ac0fad49
anish manandhar <anees.manandhar@gmail.com>
2020-06-28 15:31:30 +0545

    format sidebar content

--- sidebars.js
+++ sidebars.js
@@ -4,6 +4,38 @@ module.exports =
     "Overview": [
       "introduction"
     ],
+    "REST API": [
+      "rest-api/headers",
+      {
+        "type": "category",
+        "label": "Methods",
+        "items": [
+          "rest-api/naming-convention",
+          "rest-api/get",
+          "rest-api/post",
+          "rest-api/put",
+          "rest-api/patch",
+          "rest-api/delete"
+        ]
+      },
+      "rest-api/security",
+      "rest-api/versioning",
+      "rest-api/hateaos"
+    ],
+    "Github": [
+      "git/branch_naming_convention",
+      "git/branching_strategy",
+      "git/smart_commit",
+      {
+        "type": "category",
+        "label": "Release Management",
+        "items": [
+          "git/tagging"
+        ]
+      },
+      "git/pull_request_best_pratices",
+      "git/code_review_checklist"
+    ],
     "Python": [
       {
         "type": "category",
@@ -41,46 +73,6 @@ module.exports =
           "java/effective-java"
         ]
       }
-    ],
-    "Naming Convention": [
-      "files",
-      "classes",
-      "functions",
-      "variables",
-      "constants",
-      "folders"
-    ],
-    "REST API": [
-      "rest-api/headers",
-      {
-        "type": "category",
-        "label": "Methods",
-        "items": [
-          "rest-api/naming-convention",
-          "rest-api/get",
-          "rest-api/post",
-          "rest-api/put",
-          "rest-api/patch",
-          "rest-api/delete"
-        ]
-      },
-      "rest-api/security",
-      "rest-api/versioning",
-      "rest-api/hateaos"
-    ],
-    "Github": [
-      "git/branch_naming_convention",
-      "git/branching_strategy",
-      "git/smart_commit",
-      {
-        "type": "category",
-        "label": "Release Management",
-        "items": [
-          "git/tagging"
-        ]
-      },
-      "git/pull_request_best_pratices",
-      "git/code_review_checklist"
     ]
   }
 }
